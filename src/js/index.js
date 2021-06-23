f70f6cb48f24a1ca6cd29e7873fee8ba703ab221
Maks <maks.pronevich@gmail.com>
2021-06-23 15:16:43 +0300

    modify file

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,17 @@ import './modules/slider';
 window.addEventListener('DOMContentLoaded', () => {
 	tabs();
 });
+
+const leftMenu = document.querySelector('.left-menu');
+const toggleBtn = document.querySelector('.left-menu__toggle-btn');
+const links = document.querySelectorAll('.left-menu__item');
+
+links.forEach(link =>
+	link.addEventListener('click', () => {
+		leftMenu.classList.remove('active');
+	}),
+);
+
+toggleBtn.addEventListener('click', () => {
+	leftMenu.classList.toggle('active');
+});
