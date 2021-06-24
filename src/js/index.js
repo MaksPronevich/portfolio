c852dab9d26bfe724a8760021d591345363e3930
Maks <maks.pronevich@gmail.com>
2021-06-24 17:03:02 +0300

    modify files

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import tabs from './modules/tabs';
-import './modules/left-menu';
 import './modules/slider';
+import './modules/left-menu';
 
 window.addEventListener('DOMContentLoaded', () => {
 	tabs();
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,17 +1,24 @@
 import Swiper, { Navigation, Pagination, Autoplay } from 'swiper/core';
 Swiper.use([Navigation, Pagination, Autoplay]);
 
-const swiper = new Swiper('.portfolio-slider', {
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
+const swiper = new Swiper('.swiper-container', {
+	observer: true,
+	observeParents: true,
+	observeSlideChildren: true,
+	// loop: true,
 
 	pagination: {
 		el: '.swiper-pagination',
+		clickable: true,
+	},
+
+	navigation: {
+		nextEl: '.slider-button-next',
+		prevEl: '.slider-button-prev',
 	},
 
 	autoplay: {
-		delay: 3000,
+		delay: 1000,
+		pauseOnMouseEnter: true,
 	},
 });
